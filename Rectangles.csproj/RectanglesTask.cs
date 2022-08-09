591c1f3eeb713e526f6ae08484918af8b98585cf
gvarys <johnnyzaev@gmail.com>
2022-08-09 14:19:28 +0300

    Intersection done!

--- Rectangles.csproj/RectanglesTask.cs
+++ Rectangles.csproj/RectanglesTask.cs
@@ -8,13 +8,28 @@ namespace Rectangles
 		public static bool AreIntersected(Rectangle r1, Rectangle r2)
 		{
 			// так можно обратиться к координатам левого верхнего угла первого прямоугольника: r1.Left, r1.Top
-			return true;
+			
+			return Math.Max(r1.Top, r2.Top) <= Math.Min(r1.Bottom, r2.Bottom) &&
+			       Math.Max(r1.Left, r2.Left) <= Math.Min(r1.Right, r2.Right);
 		}
 
 		// Площадь пересечения прямоугольников
 		public static int IntersectionSquare(Rectangle r1, Rectangle r2)
 		{
-			return 0;
+			if (!AreIntersected(r1, r2))
+				return 0;
+			int left = Math.Max(r1.Left, r2.Left);
+			int top = Math.Max(r1.Top, r2.Top);
+			int right = Math.Min(r1.Right, r2.Right);
+			int bottom = Math.Min(r1.Bottom, r2.Bottom);
+
+			int width = right - left;
+			int height = bottom - top;
+
+			if (width < 0 || height < 0)
+				return 0;
+
+			return width * height;
 		}
 
 		// Если один из прямоугольников целиком находится внутри другого — вернуть номер (с нуля) внутреннего.
