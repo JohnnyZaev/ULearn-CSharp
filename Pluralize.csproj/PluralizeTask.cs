f96030a2e7855a5d09943d81ffa1f0af8bc26974
gvarys <johnnyzaev@gmail.com>
2022-08-09 11:35:33 +0300

    PluralizeTask done!

---
+++ Pluralize.csproj/PluralizeTask.cs
@@ -0,0 +1,14 @@
+﻿namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+			if ((count > 10 && count < 20) || (count % 100 > 10 && count % 100 < 20) )  return "рублей";
+			var temp = count % 10;
+			if (temp == 1) return "рубль";
+			if (temp > 1 && temp < 5) return "рубля";
+			return "рублей";
+		}
+	}
+}
